17cbaa155b75281c96f47e2a7b1c52c1b41c32ef
Marco <marcoregina92@gmail.com>
2023-11-30 15:38:47 +0100

    Imposta cartelle e installa axios

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+import { store } from "./store.js"
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+
+<template></template>
+
+
+
+
+
+<style scoped></style>
