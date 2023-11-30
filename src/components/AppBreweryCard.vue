8696dc36840da232e032ca1e828f1f1065e712fe
Marco <marcoregina92@gmail.com>
2023-11-30 17:33:20 +0100

    style card

--- src/components/AppBreweryCard.vue
+++ src/components/AppBreweryCard.vue
@@ -9,15 +9,24 @@ export default {
 </script>
 
 <template>
-    <h1>{{ brewery.name }}</h1>
-    <h3>{{ "Country: " + brewery.country }}</h3>
-    <h3>{{ "City: " + brewery.city }}</h3>
-    <h3>{{ "Phone: " + brewery.phone }}</h3>
-    <h3>{{ "Website: " + brewery.website_url }}</h3>
+    <div class="card">
+        <h1>{{ brewery.name }}</h1>
+        <h3>{{ "Country: " + brewery.country }}</h3>
+        <h3>{{ "City: " + brewery.city }}</h3>
+        <h3>{{ "Phone: " + brewery.phone }}</h3>
+        <h3>{{ "Website: " + brewery.website_url }}</h3>
+    </div>
 </template>
 
 <style scoped>
-h2 {
-    color: white;
+.card {
+    width: calc(100% / 5 - 16px);
+    background-color: goldenrod;
+    padding: 3px;
+    margin: 5px;
+}
+
+h3 {
+    color: rgb(58, 54, 54)
 }
 </style>
